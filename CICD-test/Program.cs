fb540b89cf3a62a52a8eb88b6145322011428f5f
sarahwinroth <sarahwinroth@hotmail.com>
2021-11-05 11:32:18 +0100

    added error for test

--- CICD-test/Program.cs
+++ CICD-test/Program.cs
@@ -6,7 +6,7 @@ namespace CICD_test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!")
         }
     }
 }
